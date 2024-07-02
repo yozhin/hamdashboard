1acc1621823aecb8888521d3884ceef9c39b86ac
yozhin <46226633+yozhin@users.noreply.github.com>
2024-07-02 15:42:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const topBarCenterText = `VA3HDL - FN04ga`;
+const topBarCenterText = `KF8BFM - EN91dl`;
 // Menu items
 // Structure is as follows HTML Color code, Option, target URL, scaling 1=Original Size, side (optional, nothing is Left, "R" is Right)
 // The values are [color code, menu text, target link, scale factor, side],
